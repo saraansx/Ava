06d4012dddc1804cf41e1f7d2329cd12ec79fffc
saraansx <zade.wuwa@gmail.com>
2025-12-15 18:49:13 +0530

    aa

--- managers/tool_manager.py
+++ managers/tool_manager.py
@@ -34,10 +34,12 @@ class ToolManager:
         if "news" in text:
             return self.tools.get("news")
         
-        vision_keywords = ["see", "holding", "look at", "describe", "vision"]
+        vision_keywords = ["see", "holding", "look at", "describe", "vision", "dikh", "dekho", "najar"]
         if any(keyword in text for keyword in vision_keywords):
             if "you see" in text or "do you see" in text or "am i holding" in text or "look at this" in text:
                  return self.tools.get("vision")
+            if "kya dikh" in text or "dikh rha" in text or "isko dekho" in text or "najar aa rha" in text:
+                return self.tools.get("vision")
         
         system_keywords = [
             "system", "spec", "specs", "processor", "cpu", 
